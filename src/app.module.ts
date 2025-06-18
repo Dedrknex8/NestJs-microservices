f4e50946968357714344b8cd29fc7a5b95d0fbb0
Rohit Tiwari <166986989+Dedrknex8@users.noreply.github.com>
2025-06-18 20:02:52 +0530

    Added auth service and post service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,43 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal : true
+    }),
+    TypeOrmModule.forRoot({
+      type : 'postgres',
+      host : 'localhost',
+      port : 5432 ,
+      username  : 'postgress',
+      password  :'root',
+      database : ''
+    }),
+    ClientsModule.register([
+      {
+        name : 'Service_A',
+        transport : Transport.TCP,
+        options : {
+          host : 'localhost',
+          port : 3001,
+        },
+      },
+      {
+        name : 'Service_B',
+        transport : Transport.TCP,
+        options : {
+          host : 'localhost',
+          port : 3002
+        }
+      }
+
+    ])
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
