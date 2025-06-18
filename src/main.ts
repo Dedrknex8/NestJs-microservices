a9934e3045973fad3d167274193fe43d328992fd
Rohit Tiwari <166986989+Dedrknex8@users.noreply.github.com>
2025-06-18 22:15:24 +0530

    added maint.ts configuration for auth microservice

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule,{
-    transport : Transport.TCP,
-    options : {
-      host : 'localhost',
-      port : '3001'
-    } 
-  });
+  const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
