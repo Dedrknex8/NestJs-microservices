6dd5ea59dc9de44f5cff9e8bec480c27d17178fc
Rohit Tiwari <166986989+Dedrknex8@users.noreply.github.com>
2025-06-19 17:49:39 +0530

    create auth service controller and module

---
+++ auth-service/src/auth/DTO/user.login.dto.ts
@@ -0,0 +1,18 @@
+
+
+import { IsEmail, IsNotEmpty, IsString, MinLength } from "class-validator";
+
+
+export class LoginDto{
+    @IsNotEmpty()
+    @IsEmail({},{message : "Email is required"})
+    email : string
+
+    @IsNotEmpty({message : "Password cannot be empty"})
+    @IsString({message: 'Password must be a string'})
+   
+    
+    password:string
+
+
+}
