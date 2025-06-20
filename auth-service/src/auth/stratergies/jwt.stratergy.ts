f19280c281d2b572791d7dd2023fbc22fc50b9f4
Rohit Tiwari <166986989+Dedrknex8@users.noreply.github.com>
2025-06-20 22:06:59 +0530

    implemented jwt startergy and auth guard in authservice

---
+++ auth-service/src/auth/stratergies/jwt.stratergy.ts
@@ -0,0 +1,32 @@
+import { Injectable, UnauthorizedException } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { PassportModule,PassportStrategy } from "@nestjs/passport";
+import { ExtractJwt,Strategy } from "passport-jwt";
+import { AuthService } from "src/auth/auth.service";
+
+
+@Injectable()
+export class JwtStartergy extends PassportStrategy(Strategy){ 
+    constructor(
+        private authService:AuthService,
+        private configservice:ConfigService){
+        super({
+            jwtFromRequest : ExtractJwt.fromAuthHeaderAsBearerToken(),
+            ignoreExpiration : false,
+            secretOrKey: configservice.getOrThrow<string>('JWT_SECRET')
+        });
+    }
+
+    async validate(payload:any){
+        try {
+            const user = await this.authService.getUserById(payload)
+           
+            return {
+                ...user,
+                roke : user.role
+            }
+        } catch (error) {
+            throw new UnauthorizedException('Invalid-Token')
+        }
+    }
+}
