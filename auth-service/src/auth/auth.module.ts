99206f3de2efcf463ae754c259fe7e8b899b79c1
Rohit Tiwari <166986989+Dedrknex8@users.noreply.github.com>
2025-06-20 22:11:52 +0530

    authmodule fixed

--- auth-service/src/auth/auth.module.ts
+++ auth-service/src/auth/auth.module.ts
@@ -1,4 +1,27 @@
 import { Module } from '@nestjs/common';
-
-@Module({})
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import {JwtModule, JwtService} from '@nestjs/jwt'
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtStartergy } from './stratergies/jwt.stratergy';
+import { UserEntity } from './Entities/user.entity';
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([UserEntity]),
+     JwtModule.registerAsync({
+      imports : [ConfigModule],
+      useFactory : async(configService: ConfigService)=>({
+        secret : configService.getOrThrow('JWT_SECRET'),
+        signOptions: {expiresIn: '15m'}
+      }),
+       inject: [ConfigService]
+    }),
+    ConfigModule,
+    
+  ],
+  controllers: [AuthController],
+  providers: [AuthService,JwtStartergy,],
+  exports : [AuthService,]
+})
 export class AuthModule {}
