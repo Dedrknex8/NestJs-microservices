1e98e076ce89c09d44d44ca6b42f5ea64f7530c3
Rohit Tiwari <166986989+Dedrknex8@users.noreply.github.com>
2025-06-20 22:39:35 +0530

    Create user Dto in authservice

--- auth-service/src/auth/auth.service.ts
+++ auth-service/src/auth/auth.service.ts
@@ -1,4 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Repository } from 'typeorm';
+import { Role, UserEntity as User} from './Entities/user.entity';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+        private  configService : ConfigService,
+        private userRep : Repository<User>,
+        private jwtService : JwtService
+    ){}
+
+    async registerUser(registerDto : any){
+        const userExists = await this.userRep.findOne({
+            where : {email : registerDto.email}
+        })
+
+        if(userExists){
+            throw new ConflictException('User alreay exist. Please try again with different email');
+        }
+
+        //hash the passwd
+        const hashedPassword = await bcrypt.hash
+
+        // return the save user deatils wid removing of passwd
+        const createNewUser = await this.userRep.create({
+            email  : registerDto.email,
+            username : registerDto.username,
+            password  : hashedPassword,
+            role : Role.User
+        })
+    }
+}
