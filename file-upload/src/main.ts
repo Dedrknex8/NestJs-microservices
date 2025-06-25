17d5d4d40245d00f9429b7eab53f4253e3af1fcd
Rohit Tiwari <166986989+Dedrknex8@users.noreply.github.com>
2025-06-25 18:39:44 +0530

    File service main.ts config

--- file-upload/src/main.ts
+++ file-upload/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { FileModule } from './file/file.module';
+import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.createMicroservice(FileModule,{
+    transport : Transport.TCP,
+    options : {
+      host : 'localhost',
+      port: 3003
+    }
+
+  });
+  await app.listen();
+  console.log('File service is [RUNNIG]')
 }
 bootstrap();
